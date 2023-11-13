178b8428ae959fa3d4f2ce2f201afb5eea75cf9c
WRyanKeller <rrk8058@rit.edu>
2023-11-12 23:11:18 -0500

    added slushies field to domo. adjusted css where necessary

--- client/maker.jsx
+++ client/maker.jsx
@@ -8,13 +8,14 @@ const handleDomo = e => {
 
     const name = e.target.querySelector('#domoName').value;
     const age = e.target.querySelector('#domoAge').value;
+    const slushies = e.target.querySelector('#domoSlushies').value;
 
-    if (!name || !age) {
+    if (!name || !age || !slushies) {
         helper.handleError('All fields are required!');
         return false;
     }
 
-    helper.sendPost(e.target.action, {name, age}, loadDomosFromServer);
+    helper.sendPost(e.target.action, {name, age, slushies}, loadDomosFromServer);
 
     return false;
 };
@@ -32,6 +33,8 @@ const DomoForm = props => {
             <input id='domoName' type='text' name='name' placeholder='Domo Name' />
             <label htmlFor='age'>Age: </label>
             <input id='domoAge' type='number' min='0' name='age' />
+            <label htmlFor='slushies'>Slushies: </label>
+            <input id='domoSlushies' type='number' min='0' name='slushies' />
             <input className='makeDomoSubmit' type='submit' value='Make Domo' />
         </form>
     )
@@ -52,6 +55,7 @@ const DomoList = props => {
                 <img src='/assets/img/domoface.jpeg' alt='domo face' className='domoFace' />
                 <h3 className='domoName'> Name: {domo.name} </h3>
                 <h3 className='domoAge'> Age: {domo.age} </h3>
+                <h3 className='domoSlushies'> Slushies: {domo.slushies} </h3>
             </div>
         );
     });
--- server/controllers/Domo.js
+++ server/controllers/Domo.js
@@ -7,22 +7,23 @@ const makerPage = async (req, res) => {
 };
 
 const makeDomo = async (req, res) => {
-  if (!req.body.name || !req.body.age) {
+  if (!req.body.name || !req.body.age || !req.body.slushies) {
     return res.status(400).json({
-      error: 'Both name and age are required!',
+      error: 'All fields are required!',
     });
   }
 
   const domoData = {
     name: req.body.name,
     age: req.body.age,
+    slushies: req.body.slushies,
     owner: req.session.account._id,
   };
 
   try {
     const newDomo = new Domo(domoData);
     await newDomo.save();
-    return res.status(201).json({ name: newDomo.name, age: newDomo.age });
+    return res.status(201).json({ name: newDomo.name, age: newDomo.age, slushies: newDomo.slushies });
   } catch (err) {
     console.log(err);
     if (err.code === 11000) {
@@ -42,7 +43,7 @@ const getDomos = async (req, res) => {
     const query = {
       owner: req.session.account._id,
     };
-    const docs = await Domo.find(query).select('name age').lean().exec();
+    const docs = await Domo.find(query).select('name age slushies').lean().exec();
 
     return res.json({ domos: docs });
   } catch (err) {
